f34344906587a573c43ea17d7cb377b93dc37b30
Arpan Neupane <arpanneupane19@gmail.com>
2021-02-23 10:55:43 -0600

    registration validation

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from flask_wtf.file import FileField, FileAllowed
 from flask_bootstrap import Bootstrap
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import InputRequired, Email, Length, ValidationError
+from models import *
 
 
 class LoginForm(FlaskForm):
@@ -20,3 +21,16 @@ class RegisterForm(FlaskForm):
         min=4, max=15)], render_kw={"placeholder": "Username"})
     password = PasswordField("Password", validators=[InputRequired(), Length(
         min=4, max=15)], render_kw={"placeholder": "Password"})
+
+    def validate_username(self, username):
+        existing_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_username:
+            raise ValidationError(
+                'That username already exists, please choose a different one.')
+
+    def validate_email(self, email):
+        existing_email = User.query.filter_by(email=email.data).first()
+        if existing_email:
+            raise ValidationError(
+                'That email already exists, please choose a different one.')
